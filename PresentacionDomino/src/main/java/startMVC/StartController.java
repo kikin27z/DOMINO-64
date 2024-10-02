c9c7cef5b386f4ac663216b9f69c0145484b529d
luisa <moraLF20@outlook.com>
2024-10-01 19:07:49 -0700

    proyecto presentacion v1

---
+++ PresentacionDomino/src/main/java/startMVC/StartController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package startMVC;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author luisa
+ */
+public class StartController {
+    StartView view;
+    StartModel model;
+    
+    public StartController(StartView view, StartModel model){
+        this.view = view;
+        this.model = model;
+        setListener();
+    }
+    
+    private void setListener(){
+        view.addListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciarJuego();
+            }
+        });
+    }
+    
+    private void iniciarJuego(){
+        
+    }
+}
