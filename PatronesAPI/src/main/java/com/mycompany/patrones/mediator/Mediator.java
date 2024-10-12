728ef0be110fb301ceaf624fb16648d6303b07fe
luisa <moraLF20@outlook.com>
2024-10-12 14:53:44 -0700

    proyecto de patrones

---
+++ PatronesAPI/src/main/java/com/mycompany/patrones/mediator/Mediator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.patrones.mediator;
+
+/**
+ *
+ * @author luisa M
+ */
+public interface Mediator<T> {
+    public void enviarNotificacion(T notificador);
+}
