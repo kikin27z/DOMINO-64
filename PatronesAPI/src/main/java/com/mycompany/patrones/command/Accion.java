0ea4e37b773ccfe8800fd438a40a1933711f6e31
kikin27z <karimfv04@gmail.com>
2024-10-13 08:58:46 -0700

    Arreglar errores main juntado proyectos

--- PatronesAPI/src/main/java/com/mycompany/patrones/command/Accion.java
+++ PatronesAPI/src/main/java/com/mycompany/patrones/command/Accion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.mycompany.patrones.command;
 
 /**
@@ -9,5 +5,5 @@ package com.mycompany.patrones.command;
  * @author luisa M
  */
 public interface Accion {
-    public boolean ejecutarAccion();
+    public void ejecutarAccion();
 }
