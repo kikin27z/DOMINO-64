a2556c283abab1cfc27118184c863ee4677c6b8c
luisa <moraLF20@outlook.com>
2024-10-27 12:37:40 -0700

    proyecto excepciones

---
+++ Excepciones/src/main/java/com/luisa/excepcionesLogica/LogicException.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.luisa.excepcionesLogica;
+
+/**
+ *
+ * @author luisa M
+ */
+public class LogicException extends Exception{
+
+    public LogicException() {
+    }
+
+    public LogicException(String message) {
+        super(message);
+    }
+
+    public LogicException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LogicException(Throwable cause) {
+        super(cause);
+    }
+
+    public LogicException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
